5125d0adbd54a078766dbcc7551a330b82ef359e
K. Harrison <kh296@users.noreply.github.com>
2025-06-12 14:38:38 +0100

    Request two nodes for examples

--- run_toy_example_2.3.sh
+++ run_toy_example_2.3.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=toy2.3.log    # job output file
 #SBATCH --partition=pvc9       # cluster partition to be used
 #SBATCH --account=support-gpu  # slurm project account
-#SBATCH --nodes=1              # number of nodes
+#SBATCH --nodes=2              # number of nodes
 #SBATCH --gres=gpu:4           # number of allocated gpus per node
-#SBATCH --time=00:10:00        # total run time limit (HH:MM:SS)
+#SBATCH --time=01:00:00        # total run time limit (HH:MM:SS)
 
 source lightning-setup-2.3.sh
 source run_toy_example.sh
