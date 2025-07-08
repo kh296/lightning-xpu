818bbda32152dc01b48ddefdc428dff1a8a8292e
K. Harrison <kh296@users.noreply.github.com>
2025-07-08 14:33:49 +0100

    Added initial tests

---
+++ tests/test_xpu.py
@@ -0,0 +1,17 @@
+def test_import():
+    # Test that lightning_xpu can be imported.
+    try:
+        import lightning_xpu
+        lightning_xpu_imported = True
+    except ModuleNotFoundError:
+        lightning_xpu_imported = False
+
+    assert lightning_xpu_imported == True
+
+import torch.xpu
+from lightning_xpu import XPUAccelerator
+device = XPUAccelerator()
+
+def test_xpu_available():
+    # Test that device availability is as reported by pytorch.
+    assert device.is_available() == torch.xpu.is_available()
