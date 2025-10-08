4737846926be2ac3ec95d1725be879fb50ba0304
K. Harrison <kh296@users.noreply.github.com>
2025-10-08 11:20:33 +0100

    Added test of function substitution

--- tests/test_xpu.py
+++ tests/test_xpu.py
@@ -15,3 +15,16 @@ device = XPUAccelerator()
 def test_xpu_available():
     # Test that device availability is as reported by pytorch.
     assert device.is_available() == torch.xpu.is_available()
+
+def test_distributed_modifications():
+    # Test that functions have been modified as intended.
+    from lightning_xpu.lightning.fabric.utilities.distributed import (
+            modules,
+            modified_functions,
+            )
+    assert len(modules) > 0
+    assert len(modified_functions) > 0
+    for function_name, modified_function in modified_functions.items():
+        assert callable(modified_function)
+        for module in modules:
+            assert getattr(module, function_name) is modified_function
